0dba41312b50b937af03971d58bf19a6b7172ebc
Victor Santiago <vitongos@gmail.com>
2019-06-30 15:10:49 +0200

    S3 upload.

--- scripts/demo-app-user-data.sh
+++ scripts/demo-app-user-data.sh
@@ -9,20 +9,21 @@ yum install -y git
 
 mkdir -p /var/www/demo
 
-pip install click
-pip install Flask
-pip install itsdangerous
-pip install Jinja2
-pip install MarkupSafe
-pip install nltk
-pip install oauthlib
-pip install requests
-pip install requests-oauthlib
-pip install six
-pip install textblob
-pip install tweepy --ignore-installed six requests
-pip install Werkzeug
-pip install boto3
+pip install -r requirements.txt
+# pip install click
+# pip install Flask
+# pip install itsdangerous
+# pip install Jinja2
+# pip install MarkupSafe
+# pip install nltk
+# pip install oauthlib
+# pip install requests
+# pip install requests-oauthlib
+# pip install six
+# pip install textblob
+# pip install tweepy --ignore-installed six requests
+# pip install Werkzeug
+# pip install boto3
 
 cd /var/www/demo
 git clone https://github.com/gofornaman/Real-Time-Sentiment-Analysis.git ./
--- scripts/demo-app/app.py
+++ scripts/demo-app/app.py
@@ -46,4 +46,4 @@ def tweets():
 
 
 port = int(os.environ.get('PORT', 80))
-app.run(host="0.0.0.0", port=port, debug=True)
+app.run(host="0.0.0.0", port=port, debug=False)
