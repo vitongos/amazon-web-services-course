94d6e9ada9f7376861b7b7c6693041b97018a301
Victor Santiago <vitongos@gmail.com>
2019-06-30 16:12:11 +0200

    RDS connection.

--- scripts/demo-app/app.py
+++ scripts/demo-app/app.py
@@ -48,12 +48,12 @@ def tweets():
 
 
 def store(tweets):
-    connection = psycopg2.connect(user=app.config['RDS_USER'],
+    try:
+        connection = psycopg2.connect(user=app.config['RDS_USER'],
                                         password=app.config['RDS_PASSWORD'],
                                         host=app.config['RDS_HOST'],
                                         port=app.config['RDS_PORT'],
                                         database=app.config['RDS_DATABASE'])
-    try:
         cursor = connection.cursor()
         postgres_insert_query = """ INSERT INTO search (sentiment, message, user_name) VALUES (%s,%s,%s)"""
         for record in tweets:
