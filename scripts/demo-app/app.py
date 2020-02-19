311c2a0a235c6c1643177be56271aa4d6f9250f0
Victor Santiago <vitongos@users.noreply.github.com>
2020-02-19 09:17:01 +0100

    Update app.py

--- scripts/demo-app/app.py
+++ scripts/demo-app/app.py
@@ -91,6 +91,6 @@ def store(tweets):
             app.logger.info("PostgreSQL connection is closed")
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 80))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
 
